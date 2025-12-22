368fd148dff81071fd3f1d43ca841d0795f37a23
Julio Jimenez <julio@julioj.com>
2025-12-22 03:49:47 +0000

    fix parser

--- src/parser.c
+++ src/parser.c
@@ -25,115 +25,120 @@ char **parse_line(char *line) {
     int bufsize = MAX_ARGS;
     int position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
-    char *token_start;
-    char *current;
+    char *read_pos;
     char *write_pos;
     int in_single_quote = 0;
     int in_double_quote = 0;
+    int token_start_idx = 0;
 
     if (!tokens) {
         fprintf(stderr, "%s: allocation error\n", HASH_NAME);
         exit(EXIT_FAILURE);
     }
 
-    current = line;
+    read_pos = line;
+    write_pos = line;
 
     // Skip leading whitespace
-    while (*current && isspace(*current)) {
-        current++;
+    while (*read_pos && isspace(*read_pos)) {
+        read_pos++;
+        write_pos++;
     }
 
-    while (*current) {
-        // Start of a new token
-        token_start = current;
-        write_pos = current;
+    token_start_idx = write_pos - line;
 
-        // Process characters until we hit the end of the token
-        while (*current) {
-            if (*current == '\\' && *(current + 1)) {
-                // Escape sequence
-                current++; // Skip the backslash
+    while (*read_pos) {
+        if (*read_pos == '\\' && *(read_pos + 1)) {
+            // Escape sequence
+            read_pos++; // Skip the backslash
 
-                if (in_single_quote) {
-                    // In single quotes, only \' is special
-                    if (*current == '\'') {
-                        *write_pos++ = '\'';
-                        current++;
-                    } else {
-                        // Keep backslash literal
-                        *write_pos++ = '\\';
-                        *write_pos++ = *current++;
-                    }
+            if (in_single_quote) {
+                // In single quotes, only \' is special
+                if (*read_pos == '\'') {
+                    *write_pos++ = '\'';
+                    read_pos++;
                 } else {
-                    // In double quotes or unquoted, handle common escapes
-                    switch (*current) {
-                        case '"':
-                        case '\\':
-                        case '\'':
-                            *write_pos++ = *current++;
-                            break;
-                        case 'n':
-                            *write_pos++ = '\n';
-                            current++;
-                            break;
-                        case 't':
-                            *write_pos++ = '\t';
-                            current++;
-                            break;
-                        case 'r':
-                            *write_pos++ = '\r';
-                            current++;
-                            break;
-                        default:
-                            // Unknown escape, keep both characters
-                            *write_pos++ = '\\';
-                            *write_pos++ = *current++;
-                            break;
+                    // Keep backslash literal
+                    *write_pos++ = '\\';
+                    *write_pos++ = *read_pos++;
+                }
+            } else {
+                // In double quotes or unquoted, handle common escapes
+                switch (*read_pos) {
+                    case '"':
+                    case '\\':
+                    case '\'':
+                        *write_pos++ = *read_pos++;
+                        break;
+                    case 'n':
+                        *write_pos++ = '\n';
+                        read_pos++;
+                        break;
+                    case 't':
+                        *write_pos++ = '\t';
+                        read_pos++;
+                        break;
+                    case 'r':
+                        *write_pos++ = '\r';
+                        read_pos++;
+                        break;
+                    default:
+                        // Unknown escape, keep both characters
+                        *write_pos++ = '\\';
+                        *write_pos++ = *read_pos++;
+                        break;
+                }
+            }
+        } else if (*read_pos == '\'' && !in_double_quote) {
+            // Toggle single quote mode
+            in_single_quote = !in_single_quote;
+            read_pos++;
+        } else if (*read_pos == '"' && !in_single_quote) {
+            // Toggle double quote mode
+            in_double_quote = !in_double_quote;
+            read_pos++;
+        } else if (isspace(*read_pos) && !in_single_quote && !in_double_quote) {
+            // End of token
+            *write_pos = '\0';
+
+            // Add token to array if it's not empty
+            if (line[token_start_idx] != '\0') {
+                tokens[position] = &line[token_start_idx];
+                position++;
+
+                if (position >= bufsize) {
+                    bufsize += MAX_ARGS;
+                    tokens = realloc(tokens, bufsize * sizeof(char*));
+                    if (!tokens) {
+                        fprintf(stderr, "%s: allocation error\n", HASH_NAME);
+                        exit(EXIT_FAILURE);
                     }
                 }
-                continue;
-            } else if (*current == '\'' && !in_double_quote) {
-                // Toggle single quote mode
-                in_single_quote = !in_single_quote;
-                current++;
-                continue;
-            } else if (*current == '"' && !in_single_quote) {
-                // Toggle double quote mode
-                in_double_quote = !in_double_quote;
-                current++;
-                continue;
-            } else if (isspace(*current) && !in_single_quote && !in_double_quote) {
-                // End of token
-                break;
             }
 
+            // Skip whitespace
+            write_pos++;
+            read_pos++;
+            while (*read_pos && isspace(*read_pos)) {
+                read_pos++;
+                write_pos++;
+            }
+
+            // Start of next token
+            token_start_idx = write_pos - line;
+        } else {
             // Regular character
-            *write_pos++ = *current++;
-        }
-
-        // Null-terminate the token
-        *write_pos = '\0';
-
-        // Skip whitespace before next token
-        while (*current && isspace(*current)) {
-            current++;
-        }
-
-        // Add token to array if it's not empty
-        if (*token_start) {
-            tokens[position] = token_start;
-            position++;
-
-            if (position >= bufsize) {
-                bufsize += MAX_ARGS;
-                tokens = realloc(tokens, bufsize * sizeof(char*));
-                if (!tokens) {
-                    fprintf(stderr, "%s: allocation error\n", HASH_NAME);
-                    exit(EXIT_FAILURE);
-                }
-            }
+            *write_pos++ = *read_pos++;
         }
     }
+
+    // Handle last token
+    *write_pos = '\0';
+    if (line[token_start_idx] != '\0') {
+        tokens[position] = &line[token_start_idx];
+        position++;
+    }
+
     tokens[position] = NULL;
     return tokens;
 }
